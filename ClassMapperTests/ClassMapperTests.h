ccbeabfeec4c63ce70267f5455cd777ad1d4d157
Gleb Pinigin <gpinigin@gmail.com>
2013-03-10 14:21:46 +0700

    MINOR add test project

---
+++ ClassMapperTests/ClassMapperTests.h
@@ -0,0 +1,13 @@
+//
+//  ClassMapperTests.h
+//  ClassMapperTests
+//
+//  Created by Gleb on 3/10/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ClassMapperTests : SenTestCase
+
+@end
---
+++ ClassMapperTests/ClassMapperTests.m
@@ -0,0 +1,19 @@
+//
+//  ClassMapperTests.m
+//  ClassMapperTests
+//
+//  Created by Gleb on 3/10/13.
+//
+//
+
+#import "ClassMapperTests.h"
+
+@implementation ClassMapperTests
+
+- (void)testClassMappingSimple {
+    
+}
+
+
+
+@end
